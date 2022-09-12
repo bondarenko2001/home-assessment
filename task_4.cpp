487dde2dcb6556a77801e71e6c0de3bf8f023793
bondarenko2001 <103777657+bondarenko2001@users.noreply.github.com>
2022-09-12 18:28:57 +0300

    Add files via upload

---
+++ task_4.cpp
@@ -0,0 +1,111 @@
+#include <iostream>
+
+using namespace std;
+//  /* 1.1.0 Write the function that takes <name> as an argument and prints it.
+    // 1.1.0 Write the function that takes <name>, <surname> and <age> as input and prints this info.
+
+// void PrintInfoAboutMe (string name, string surname, string age) {
+//    cout << "My name is: "<<name<<endl;
+//    cout << "My surname is: "<<surname<<endl;
+//    cout << "My age is: "<<age<<endl;
+// }
+
+// int main(int argc, char const *argv[])
+// {
+
+//   string name, surname,age;
+//    cout << "What is your name:";
+// cin >> name;
+//    cout << "What is your surname:";
+// cin >> surname;
+
+//    cout << "How old are you:";
+// cin >> age;
+// cout << endl;
+
+// PrintInfoAboutMe(name,surname,age);
+// return 0;
+// }
+
+
+
+   // 1.1.1 Write the function that takes two integer numbers as an argument. It must return true if they are equal, and false in not.
+
+
+// int main(int argc, char const *argv[])
+// {
+//     int number1;
+//     int number2;
+//    cin << number1 << endl;
+//    cin << number2<< endl;
+
+//   if ( number1 == number2)
+//   {
+//  cout << "Equal";
+//   }
+//   else
+//  cout << "False";
+
+//     return 0;
+// }
+
+
+
+// int main(int argc, char const *argv[])
+
+    // 1.1.2 Write a function that takes and non-negative integer number N as an argument. 
+    //       The function must print integer numbers in the range 0 to N.
+
+
+// void print(int N)
+// {
+//     static int a = 1;
+//     cout << a << endl;
+//     if (a == N)
+//         return;
+//     ++a;
+//     print(N);
+// }
+ 
+// int main()
+// {
+//     cout << "N = ";
+//     int x;
+//     cin >> x;
+//     print(x);
+//     return 0;
+// }
+
+    // 1.1.3 Write a function that takes and non-negative integer number N as an argument. 
+    //       The function calculate sum of integer numbers in the range 0 to N, AND RETURN it.
+    // */
+    
+    // не зрозуміла різницю з попереднім
+
+  /*
+    2.1 Write a function that calculate and return the factorial of a given number as an argument. Use for or while loop. For example, if user input 3, program must print 6 (1*2*3).
+int factorial(int n);
+ */
+
+// int factorial(int a) {
+//   if(a > 1)
+//     return a * factorial(a - 1);
+//   else
+//     return 1;
+// }
+// int main() {
+
+//   int a;
+
+//   cout << "Enter a positive integer: ";
+//   cin >> a;
+
+//   cout << "Factorial of " << a << " = " << factorial (a) <<endl;
+
+
+
+
+    
+
+ 
+    
